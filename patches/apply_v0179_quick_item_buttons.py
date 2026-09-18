ea5c729f1225919fc682c9daa19b1084abaa7e00
husam4448-rgb <husam4448@gmail.com>
2026-09-18 21:55:22 +0300

    Fix v0.19.0D2B.4 build-chain declaration formatting

--- patches/apply_v0179_quick_item_buttons.py
+++ patches/apply_v0179_quick_item_buttons.py
@@ -55,7 +55,8 @@ direction_pose_d2b3a = patch_dir / "apply_v0190d2b3a_directional_pose_engine.py"
 direction_body_d2b3b = patch_dir / "apply_v0190d2b3b_directional_body_perspective.py"
 direction_head_arms_d2b3c = patch_dir / "apply_v0190d2b3c_directional_head_arms.py"
 direction_gear_d2b3d = patch_dir / "apply_v0190d2b3d_directional_gear.py"
-production_survivor_d2b4 = patch_dir / "apply_v0190d2b4_production_survivor_art.py"\n
+production_survivor_d2b4 = patch_dir / "apply_v0190d2b4_production_survivor_art.py"
+
 steps = (combined, radial, polish, static_scene, ci_compat, responsive_hud, uniform_inventory, dual_stick, qol_world, vehicle_controls, vehicle_qol, vehicle_a3, control_editor_a4, storage_b, storage_b2, universal_c, universal_c2, universal_c3, art_foundation, terrain_props, grass_density, structures_interiors, structures_parser_fix, world_coherence_c1, hud_natural_water_c2, world_density_c3, visual_fixes_c4, world_layering_c5, world_layering_c5_parser_fix, aim_occlusion_c6, storage_use_icon_d1a, bandit_loot_d1b, interior_utilities_d1c, visual_slots_d2a1, layered_visuals_d2a2, visual_d2a_parser_fix, weapon_visuals_d2b1, npc_weapon_state_d2b2, exact_weapon_icons_d2b3, character_visual_repair_d2b1, articulated_actor_d2b2a, articulated_gear_d2b2b, animation_integration_d2b2c, weapon_pose_fix_d2b2d, slim_weapon_art_d2b2e, direction_pose_d2b3a, direction_body_d2b3b, direction_head_arms_d2b3c, direction_gear_d2b3d, production_survivor_d2b4)
 for step in steps:
     if not step.is_file():
